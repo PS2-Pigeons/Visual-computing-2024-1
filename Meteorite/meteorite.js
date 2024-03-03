76d34862f3a00b3b162235aa6d2e776be6a69650
DysDan <dtobarl@unal.edu.co>
2024-03-03 13:19:55 -0500

    Implemented projectiles
    a

--- Meteorite/meteorite.js
+++ Meteorite/meteorite.js
@@ -1,13 +1,18 @@
 // TAREA asteroids
 
-let ship;
+let ship, projectile;
+var lasers = []; // Array de todos los laseres existentes
+let attackCooldown = 1000 // 1s de cooldown para atacar
+let lastAttackTime = -attackCooldown //Se deja así por como se maneja la duración de projectiles
+let projectileLifeSpan = 3000 // Tiempo que queda vivo un projectil
+let projectileSpeedMultiplier = 5
 
 function setup() {
   var myCanvas = createCanvas(600, 600);
   // TESTING
   // Comentar siguiente linea para probar en vscode con extensión p5canvas
   // Descomentar para que quede en orden las cosas en el html deployeado / Live server
-  myCanvas.parent("canvasContainer");
+  //myCanvas.parent("canvasContainer");
 
   frameRate(60);
   ship = new Ship();
@@ -18,6 +23,41 @@ function draw() {
   background(0);
   ship.update();
   ship.draw();
+}
+
+class Projectile{
+  constructor(){
+    this.rotation = ship.rotation;
+    this.pos = createVector(ship.pos.x,ship.pos.y)
+    this.velocity = createVector(cos(this.rotation)*projectileSpeedMultiplier,sin(this.rotation)*projectileSpeedMultiplier);
+    this.spawnTime = millis()
+    this.timeAlive = 0
+  }
+  update(){
+    this.pos.x += this.velocity.x
+    this.pos.y += this.velocity.y
+    this.timeAlive = millis()
+    if (this.pos.x > width){
+      this.pos.x = 0;
+    }
+    if (this.pos.x < 0){
+      this.pos.x = width;
+    }
+    if (this.pos.y > height){
+      this.pos.y = 0;
+    }
+    if (this.pos.y < 0){
+      this.pos.y = height;
+    }
+
+  }
+  draw(){
+    push();
+    stroke(255);
+    strokeWeight(4);
+    point(this.pos.x,this.pos.y);
+    pop();
+  }
 }
 
 class Ship{
@@ -42,7 +82,15 @@ class Ship{
     if (!keyIsDown(UP_ARROW)){
       this.impulse = false;
     }
-
+    if (keyIsDown(DOWN_ARROW)) { 
+      if (millis() - lastAttackTime >= attackCooldown ){
+        lastAttackTime = millis()
+      // Coso pa crear projectil y disparar
+        projectile = new Projectile
+        var i = lasers.length
+        lasers[i] = projectile
+      }
+    }
     this.pos.x += this.velocity.x;
     this.pos.y += this.velocity.y;
     // Loop en el borde de la pantalla
@@ -70,6 +118,7 @@ class Ship{
       this.rotation = 360;
     }
   }
+
   goForward(){
     // añadir fuerza segun rotacion actual de la nave
     this.force.x = cos(this.rotation)/10;
@@ -93,10 +142,19 @@ class Ship{
     
   }
 
+
+
   draw(){
     push();
     stroke(255);
     noFill();
+    for (var i = 0; i < lasers.length; i++){
+      lasers[i].update();
+      lasers[i].draw();
+      if (lasers[i].timeAlive - lasers[i].spawnTime >= projectileLifeSpan){
+        lasers.splice(i,1);
+      }
+    }
     translate(this.pos.x, this.pos.y);
     rotate(this.rotation);
     // Nave
