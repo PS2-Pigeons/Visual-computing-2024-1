de4953696496eb98417bb5c5b8769f7a06760404
Santiago Reyes <sreyeso@unal.edu.co>
2024-04-04 13:50:33 -0500

    Squashed commit of the following:
    commit f139ad7a8a74a3f7f47168353c77e325f9e116eb
    Author: SebMatDo <59572442+SebMatDo@users.noreply.github.com>
    Date:   Thu Apr 4 10:05:08 2024 -0500
    
        Borrado y movimiento de las capas en coordenadas Y luego de llenar un plano XZ con bloques
    
    commit 86f26c6f30317f476c3339a439e1dc463a417eed
    Author: Santiago Reyes <sreyeso@unal.edu.co>
    Date:   Wed Apr 3 12:51:56 2024 -0500
    
        changed collision detection logic
    
        with that also changes to the gamematrix element definition
    
    commit 24b47f5e3207542b76b944b308f71d16ad1783c3
    Author: Santiago Reyes <sreyeso@unal.edu.co>
    Date:   Wed Apr 3 11:47:57 2024 -0500
    
        fixed axis
    
        changed transparent tiles
    
    commit 52b832d413627492080b58295eb73a69886df013
    Author: SebMatDo <59572442+SebMatDo@users.noreply.github.com>
    Date:   Wed Apr 3 08:42:49 2024 -0500
    
        p5canvas vuelve a funcionar con un par de cambios
    
    commit 90e165345942efb9fcffcb3c52eb6fe7632dbef6
    Author: Santiago Reyes <sreyeso@unal.edu.co>
    Date:   Wed Apr 3 00:59:21 2024 -0500
    
        Delete tetristest.js
    
    commit 4a99a456c7bed4582e01cea8055ba63bcb5b47eb
    Author: Santiago Reyes <sreyeso@unal.edu.co>
    Date:   Wed Apr 3 00:58:45 2024 -0500
    
        refactor
    
        check TODOs thx
        a mimir
    
    commit e4c704446c94e9a1bda4e84086ba2a3e5cbcce50
    Merge: 644b6ad ac543e2
    Author: Santiago Reyes <sreyeso@unal.edu.co>
    Date:   Tue Apr 2 22:20:04 2024 -0500
    
        Merge branch 'Index' into Tetris3D
    
    commit 644b6ad31afb52f75984663ff85e56d341fd7447
    Merge: 2142cc9 66f3d0c
    Author: Santiago Reyes <sreyeso@unal.edu.co>
    Date:   Tue Apr 2 22:19:52 2024 -0500
    
        Merge branch 'ArcadeGame' into Tetris3D
    
    commit 2142cc9c86a61df64ef052ed12258c0aba216360
    Author: SebMatDo <59572442+SebMatDo@users.noreply.github.com>
    Date:   Tue Apr 2 21:51:36 2024 -0500
    
        Base poderosa para el tetris
    
    commit 97db58c82c4891cef86b871fc9c659388c15c6f7
    Author: Santiago Reyes <sreyeso@unal.edu.co>
    Date:   Tue Apr 2 12:22:22 2024 -0500
    
        grid refactor and camera controls
    
        epic camera controls ebin con zoom y panning, reset de todo y border del canvas
    
    commit 1560460247bb5749b616166ef4634faa7ecfd166
    Author: Santiago Reyes <sreyeso@unal.edu.co>
    Date:   Sun Mar 3 11:38:15 2024 -0500
    
        epic code formatting
    
        TLDR ;
    
    commit dc3e8ff114caa8bc8c7e59646d474f95e6556a3e
    Author: SebMatDo <59572442+SebMatDo@users.noreply.github.com>
    Date:   Sun Mar 3 11:22:52 2024 -0500
    
        Cubos caer
    
        probar colisiones

---
+++ Tetris3D/Logic/gameLogic.js
@@ -0,0 +1,370 @@
+let matrixWidth = (8) + 2;  // Width of the playable area + 2 (borders)
+let matrixHeight = (12) + 2; // Height of the playable area + 2 (borders)
+let matrixDepth = (8) + 2;  // Depth of the playable area + 2 (borders)
+let boxSize = 25;     // Size of each box
+let gameMatrix = [];    // 3D array to store colors
+let lineLengthX = matrixWidth * boxSize;
+let lineLengthY = matrixHeight * boxSize;
+let blackLineZ = matrixDepth * boxSize;
+let cam;
+let currentBlock;
+let timeInterval = 60;
+
+
+let zoomLevel = 1;    // Zoom level
+let horiRotation = 0;    // Horizontal rotation
+let vertRotation = 0;    // Vertical rotation
+
+function setup() {
+  var myCanvas = createCanvas(800, 600, WEBGL); // Create a WebGL canvas 
+  // TESTING
+  // Comentar siguiente linea para probar en vscode con extensión p5canvas
+  // Descomentar para que quede en orden las cosas en el html deployeado / Live server
+  // myCanvas.parent("canvasContainer");
+  
+  frameRate(60);
+  angleMode(DEGREES);
+
+  cam = createCamera();
+  cam.camera(lineLengthX * 1.55, lineLengthY * 1.85, blackLineZ * 1.55 , boxSize, boxSize, boxSize, 0, -1, 0);
+  cam.perspective(40);
+
+  // Initialize the 3D matrix
+  for (let i = 0; i < matrixWidth; i++) {
+    gameMatrix[i] = [];
+    for (let j = 0; j < matrixHeight; j++) {
+      gameMatrix[i][j] = [];
+        for (let k = 0; k < matrixDepth; k++) {
+          let col = null;
+          if (i == 0 || i == matrixWidth - 1 || j == 0 || j == matrixHeight - 1 || k == 0 || k == matrixDepth-1) col = 'gray'; // side borders
+          if (j == matrixHeight - 1) col = 'purple'; // top border
+          gameMatrix[i][j][k] = col;
+        }
+    }
+  }
+  // Descomentar la siguiente linea para que aparezca un nivel prehecho
+  // testingSetup();
+  currentBlock = new Tetromino([[0,0,0], [1,0,0], [0,0,1], [1,0,1],[0,-1,0], [1,-1,0], [0,-1,1], [1,-1,1],[0,-2,0], [1,-2,0], [0,-2,1], [1,-2,1]] , "green");
+  
+}
+
+function draw() {
+  background(255);
+  //Camera controls
+  rotateX(-vertRotation);
+  rotateZ(vertRotation);
+  rotateY(horiRotation);
+
+  // Draw 3D matrix
+  for (let i = 0; i < matrixWidth - 1 ; i++) {
+    for (let j = 0; j < matrixHeight - 1; j++) {
+        for (let k = 0; k < matrixDepth - 1; k++) {
+                if (gameMatrix[i][j][k]) {
+                  push(); // Save the current transformation matrix
+                    translate(i * boxSize, j * boxSize, k * boxSize);
+                    fill(gameMatrix[i][j][k]);
+                    box(boxSize);
+                    pop(); // Restore the previous transformation matrix
+                }
+        }
+    }
+  }
+
+  // Timer to control the update function
+  if (frameCount % timeInterval === 0) update();
+  if (currentBlock) currentBlock.draw();
+
+}
+
+function keyPressed(){
+  //Movimiento lateral tetrominos, flechas y WASD
+  if (keyCode == LEFT_ARROW || keyCode == 65 && currentBlock != null)   currentBlock.move(1,0);
+  if (keyCode == RIGHT_ARROW || keyCode == 68 && currentBlock != null)  currentBlock.move(-1,0);
+  if (keyCode == DOWN_ARROW || keyCode == 83 && currentBlock != null)   currentBlock.move(0,1);
+  if (keyCode == UP_ARROW || keyCode == 87 && currentBlock != null)     currentBlock.move(0,-1);
+
+  //Rotaciones tetrominos
+  if (keyCode == 72 /* H */ && currentBlock != null)                    currentBlock.rotate(1,0,0);
+  if (keyCode == 74 /* J */&& currentBlock != null)                     currentBlock.rotate(0,1,0);
+  if (keyCode == 75 /* K */&& currentBlock != null)                     currentBlock.rotate(0,0,1);
+
+  //Resetear camara
+  if (key == ' ' /* Spacebar */) {
+    horiRotation = 0;
+    vertRotation = 0;
+    zoomLevel = 1;
+    cam.perspective(40);
+  }
+
+}
+
+function update(){
+  if (currentBlock != null){
+    currentBlock.update();
+  }
+  else{ // If block is null then throw the next one
+    currentBlock = new Tetromino([[0,0,0], [1,0,0], [0,0,1], [1,0,1]], "green");
+  }
+}
+
+// UTILITY FUNCTIONS TO DO MATH MAGIC
+
+function matrixMult(A, B) {
+  if(A[0].length !== B.length) return "A col != B row";
+  let l = A.length;      // Number of rows in A
+  let m = A[0].length;   // Number of columns in A and number of rows in B
+  let n = B[0].length;   // Number of columns in B
+  
+  let C = [];
+  for(let i = 0; i < l; i++){
+    C[i] = [];
+    for(let j = 0; j < n; j++){
+      C[i][j] = [];
+    }
+  }
+  
+  for(let row = 0; row < l ; row++){
+    for(let col = 0; col < n; col++){
+      let v = [];
+      let w = [];
+      for(let i = 0; i < m ; i++){
+        v.push(A[row][i]);
+        w.push(B[i][col]);
+      }
+      C[row][col] = dot(v,w);
+    }
+  }
+  return C;
+}
+
+function dot(v, w){
+  if(v.length != w.length) return "Error, vector lengths do not match";
+  let sum = 0;
+  for(let i = 0; i < v.length; i++){
+    sum += v[i] * w[i];
+  }
+  return sum;
+}
+
+function compareArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) if (arr1[i] !== arr2[i]) return false;
+  return true;
+}
+
+function mouseDragged(){
+  let dx = mouseX - pmouseX;
+  let dy = mouseY - pmouseY;
+  // TODO: Elemento dom o algo para poder invertir la camara, modificar signo
+  horiRotation = constrain(horiRotation - /* este signo xd */ dx * 0.05, -25 * zoomLevel, 25 * zoomLevel);
+  vertRotation = constrain(vertRotation - /* este signo xd */ dy * 0.05, -4.5 * zoomLevel, 12 * zoomLevel);
+}
+
+function mouseWheel(event) {
+  zoomLevel += event.delta * 0.003;
+  zoomLevel = constrain(zoomLevel, 0.5, 1.5);
+  cam.perspective(40 * zoomLevel);
+}
+
+function moveYLayers(yLayer) {
+  // We ignore borders and max move to height - 2
+  for (let y = yLayer; y < matrixHeight - 2; y++) {
+    for (let x = 1; x < matrixWidth - 1; x++) {
+        for (let z = 1; z < matrixDepth - 1; z++) {
+          gameMatrix[x][y][z] = gameMatrix[x][y+1][z]; // Moving layers
+        }
+      }
+    }
+}
+
+function testingSetup(){
+// Llenar bloques en el setup para destruirlos facilmente
+for (let i = 1; i < matrixWidth - 3; i++) {
+  for (let k = 1; k < matrixDepth - 2; k++) {
+    let col = "blue";
+    gameMatrix[i][1][k] = col;
+  }
+}
+for (let i = 1; i < matrixWidth - 1; i++) {
+  for (let k = 4; k < matrixDepth - 1; k++) {
+    let col = "blue";
+    gameMatrix[i][1][k] = col;
+  }
+}
+for (let i = 1; i < matrixWidth - 3; i++) {
+  for (let k = 1; k < matrixDepth - 2; k++) {
+    let col = "blue";
+    gameMatrix[i][2][k] = col;
+  }
+}
+for (let i = 1; i < matrixWidth - 1; i++) {
+  for (let k = 3; k < matrixDepth - 1; k++) {
+    let col = "blue";
+    gameMatrix[i][2][k] = col;
+  }
+}
+for (let i = 1; i < matrixWidth - 3; i++) {
+  for (let k = 3; k < matrixDepth - 3; k++) {
+    let col = "red";
+    gameMatrix[i][3][k] = col;
+  }
+}
+}
+
+class Tetromino {
+  constructor(vertexMatrix, color = "blue") {
+    this.tetronimoMatrix = vertexMatrix;
+    this.col = color;
+    this.origin = [1, matrixHeight - 2, 1];
+    this.falling = true;
+  }
+
+  draw() {
+    push();
+      translate(boxSize * this.origin[0],boxSize * this.origin[1], boxSize * this.origin[2]); //Moverse al origen de la ficha 
+      fill(this.col);
+      for (let vert of this.tetronimoMatrix) {
+        push(); 
+          translate(boxSize * vert[0], boxSize * vert[1], boxSize * vert[2]); //moverse a las coordenadas relativas del vértice
+          box(boxSize, boxSize);
+        pop();
+      }
+    pop();
+}
+
+  update() {
+      if (this.falling) {
+        let emulOrigin = [this.origin[0], this.origin[1] - 1, this.origin[2]]; // Falling position in the next update call
+        let simVertPositions = this.matrixsumPosition(this.tetronimoMatrix, emulOrigin);
+
+        if (this.checkCollision(simVertPositions)) {
+
+            // TODO: Matar la pieza dsp de un tiempo, no instantaneamente
+            this.falling = false;
+
+            // push every Cube into the gamematrix
+            for (let coord of this.matrixsumPosition(this.tetronimoMatrix,this.origin)) gameMatrix[coord[0]][coord[1]][coord[2]] = this.col;
+            currentBlock = null;
+            
+            //TODO: Delete complete matrix in xy
+            
+            // Check if a row is full to empty
+            // Filter by Y layer and count if it has all the blocks in there.
+            // This is for optimization purpouses, other ways to do this require a lot of comparison
+            
+            // We ignore: all the grey and purple box
+            for (let y = 1; y < matrixHeight - 1; y++) {
+              let yLayerCount = 0;
+              for (let x = 1; x < matrixWidth - 1; x++) {
+                  for (let z = 1; z < matrixDepth - 1; z++) {
+                      if (gameMatrix[x][y][z] !== null) {
+                          yLayerCount += 1; // Count blocks of the yLayer not null
+                      }
+                  }
+              }
+              if (yLayerCount == (matrixWidth-2)*(matrixDepth-2)){ // length ignoring borders
+                //Delete actual y layer then move others
+                moveYLayers(y);
+                y--; // We going back to see if after the movement of layers there is another one to move in the new position
+              }
+            }
+            // CHECK if some block in y =0, if true game over. This could also be done in current block
+        }else{
+          this.origin[1] -= 1;
+        }
+      }
+  }
+
+  move(x, z) {
+     // Check if we can move to desired position without colliding before actually moving, simulating collision before
+      let emulOrigin = [this.origin[0] + x, this.origin[1] , this.origin[2] + z];
+      let simVertPositions = this.matrixsumPosition(this.tetronimoMatrix, emulOrigin);
+
+      if (!this.checkCollision(simVertPositions)) {
+        this.origin[0] += x;
+        this.origin[2] += z;
+      } else {
+        // TODO: Indicador que diga que ahi no se puede mover, cambiar el color slightly maybe
+      }
+
+  }
+
+  checkCollision(matrix) {
+    let tetrominoVertices = new Set(matrix.map(vertex => vertex.join(',')));
+
+    for (let j = 0; j < matrixHeight; j++) {
+        for (let i = 0; i < matrixWidth; i++) {
+            for (let k = 0; k < matrixDepth; k++) {
+                if (gameMatrix[i][j][k] !== null) {
+                    if (tetrominoVertices.has([i, j, k].join(','))) return true; // Collision detected
+                }
+            }
+        }
+    }
+
+    return false;
+  }
+
+  rotate(planeX, planeY, planeZ) {
+    let rotationMatrix;
+    let newMatrix = null;
+
+    if (planeX) {
+        rotationMatrix = [
+            [1, 0, 0],
+            [0, 0, -1],
+            [0, 1, 0],
+        ];
+    } else if (planeY) {
+        rotationMatrix = [
+            [0, 0, 1],
+            [0, 1, 0],
+            [-1, 0, 0],
+        ];
+    } else if (planeZ) {
+        rotationMatrix = [
+            [0, -1, 0],
+            [1, 0, 0],
+            [0, 0, 1],
+        ];
+    }
+
+    if (rotationMatrix) {
+        let emulRot = matrixMult(this.tetronimoMatrix, rotationMatrix);
+        let matrixCol = this.matrixsumPosition(emulRot, this.origin);
+        
+        if (!this.checkCollision(matrixCol)) {
+            newMatrix = emulRot;
+        } else {
+            let message = `trata de rotar en ${planeX ? 'x' : planeY ? 'y' : 'z'} cuando colisionaria`;
+            console.log(message);
+        }
+    }
+
+    if (newMatrix) {
+        this.tetronimoMatrix = newMatrix;
+    }
+}
+
+  matrixsumPosition(vertexMatrix, origin) {
+    let simVertPositions = [];
+    for (let vert of vertexMatrix) {
+        let simVert = [
+            vert[0] + origin[0],
+            vert[1] + origin[1],
+            vert[2] + origin[2]
+        ];
+        simVertPositions.push(simVert);
+    }
+    return simVertPositions;
+}
+
+}
+
+
+// Set up the canvas and main functions
+window.setup = setup;
+window.draw = draw;
+window.keyPressed = keyPressed;
+window.mouseDragged = mouseDragged;
+window.mouseWheel = mouseWheel;
