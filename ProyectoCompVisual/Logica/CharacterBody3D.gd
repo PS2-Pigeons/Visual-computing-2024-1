c9e37e1ce7dfb2858399adcb3476f3f9486d5f15
SebMatDo <JUANSCABEZASM@hotmail.com>
2024-05-28 18:47:23 -0500

    Interactuar con consola

--- ProyectoCompVisual/Logica/CharacterBody3D.gd
+++ ProyectoCompVisual/Logica/CharacterBody3D.gd
@@ -6,35 +6,34 @@ var mouse_sensivity = 0.002
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var actualInteractionObject : Node3D = null 
-@onready var cam = $Camera3D
+@onready var cam :Camera3D = $Camera3D
 @onready var lanter = $Camera3D/Lantern
+var canMove = true
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	elif event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED and canMove:
 		if event is InputEventMouseMotion:
 			rotate_y(-event.relative.x * mouse_sensivity)
 			cam.rotate_x(-event.relative.y * mouse_sensivity)
 			#cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(-35), deg_to_rad(60))
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	move_and_slide()
-	
-	if Input.is_action_just_pressed("lanter"):
-		lanter.visible = !lanter.visible
+	if canMove:
+		if not is_on_floor():
+			velocity.y -= gravity * delta
+		var input_dir := Input.get_vector("left", "right", "up", "down")
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+		move_and_slide()
 
 func set_interaction_object(object : Node3D) -> void:
 	actualInteractionObject = object
---
+++ ProyectoCompVisual/Logica/consola.gd
@@ -0,0 +1,42 @@
+extends Node3D
+
+var isPlayerHere = false
+var playerRef : Player3D = null
+var cartridgeOn : bool = false
+const selfName = "Consola"
+
+func _input(event) -> void:
+	if isPlayerHere and Input.is_action_just_pressed("interact"):
+		if playerRef.actualInteractionObject != null \
+		and playerRef.actualInteractionObject.selfName == "CartuchoGF":
+			$cartuchoGF2.visible = true
+			playerRef.actualInteractionObject = null
+			cartridgeOn = true
+			SignalBus.change_visible_interactive_label.emit(false)
+			$Area3D.monitorable = false
+			$Area3D.monitoring = false
+			playerRef.canMove = false
+			await get_tree().create_timer(0.3).timeout
+			var tween = create_tween()
+			tween.parallel().tween_property(playerRef.cam,"rotation_degrees", Vector3(0,0,0),0.8)
+			tween.parallel().tween_property(playerRef,"rotation_degrees", Vector3(0,-180,0),0.5)
+			tween.parallel().tween_property(playerRef.cam,"position",Vector3(0,0.5,0),0.55)
+			tween.parallel().tween_property(playerRef,"position",Vector3(0.25,0.54,2.58),1.5)
+			
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is Player3D:
+		isPlayerHere = true
+		playerRef = body
+		if body.actualInteractionObject != null \
+		and body.actualInteractionObject.selfName == "CartuchoGF":
+			SignalBus.change_text_interactive_label.emit("Press E to put the videogame")
+		else:
+			SignalBus.change_text_interactive_label.emit("You need a videogame")
+		SignalBus.change_visible_interactive_label.emit(true)
+		
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body is Player3D:
+		isPlayerHere = false
+		SignalBus.change_visible_interactive_label.emit(false)
+		
